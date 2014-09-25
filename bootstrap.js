369e67b2f2109e685727f4c58c2f5fdc9ebf3c87
Steven Adams <steven@consultstevo.com>
2014-09-25 08:47:32 -0600

    Removed most of the bootstrap.

--- bootstrap.js
+++ bootstrap.js
@@ -16,20 +16,6 @@ traceur.options.privateNames = true;
 /**
  * Require the server.js which will run the application.
  */
-//Store pid
-var fs = require( "fs" );
-fs.writeFileSync( "process.pid", process.pid );
-
-//Events
-process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err);
-});
-
-process.on('SIGINT', function() {
-  console.log('Signal exit, closing.');
-  fs.unlinkSync( "process.pid" );
-  process.exit();
-});
 
 //Start app using traceur
 traceur.require( "./start.es.js" );
