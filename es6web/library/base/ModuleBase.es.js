a720e886a30394fe5cc8bd8cf8859b4d9605d530
Steven Adams <sadams@pfh.org>
2016-01-08 17:20:13 -0700

    Fixed issue with missing controller actions with babel.

--- es6web/library/base/ModuleBase.es.js
+++ es6web/library/base/ModuleBase.es.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -31,17 +31,17 @@ export class ModuleBase{
     this._InitModels();
     this._InitViews();
   }
-  
+
   get service(){
     return this._service;
   }
-  
+
   get host(){
     return this._host;
   }
-  
+
   SetupModule() { }
-  
+
   _InitControllers(){
     this._controllerDirectory = this._moduleDirectory + "/Controllers/";
     let controllerBase = this._coreLibrary.GetLib( "base/ControllerBase" );
@@ -50,7 +50,7 @@ export class ModuleBase{
       this._RebuildControllerBase();
     }
   }
-  
+
   _RebuildControllerBase( libObject ){
     let controllerLib = this._coreLibrary.GetPathListener( this._controllerDirectory );
     this._coreLibrary.AddPathListener( this._controllerDirectory, "*", null, this._AddControllerListener.bind( this ), -1 );
@@ -60,35 +60,36 @@ export class ModuleBase{
       }
     }
   }
-  
+
   _AddControllerListener( pathObject, libObject ){
     let controllerPath = libObject.path.replace( this._controllerDirectory, "" ).replace( ".es.js", "" ).toLowerCase();
     this._controllers[controllerPath] = libObject.uncompiled;
     this._FlushActionViews( controllerPath, libObject.uncompiled );
   }
-  
+
   _FlushActionViews( controllerName, controllerClass ){
     this._methodMap[controllerName] = {};
-    let mockController = new controllerClass( this, null, null );
-    for( let methodName in mockController ){
+    let methods = Object.getOwnPropertyNames( controllerClass.prototype );
+    for ( let i = 0; i < methods.length; i++ ) {
+      let methodName = methods[i];
       if( methodName[0] !== "_" && methodName.EndsWith( "Action" ) || methodName.EndsWith( "View" ) ){
         this._methodMap[controllerName][methodName.toLowerCase()] = methodName;
       }
     }
   }
-  
+
   GetController( controllerPath ){
     if( this._controllers[controllerPath] !== undefined ){
       return this._controllers[controllerPath];
     }
-    
+
     return null;
   }
-  
+
   GetModel( modelName ){
     return this._models[modelName.toLowerCase()] || null;
   }
-  
+
   GetMethod(controllerName, actionName) {
     if( actionName === null ) {
       return null;
@@ -101,12 +102,12 @@ export class ModuleBase{
     if( methodMap[actionName + "action"] ){
       return methodMap[actionName + "action"];
     }
-    
+
     return null;
   }
-  
-  
-  
+
+
+
   /**
    * Does the action requested, and fills the content buffer.
    */
@@ -121,7 +122,7 @@ export class ModuleBase{
     let defaultAction = controller.defaultAction || this._defaultAction || null;
     let defaultView = controller.defaultView || this._defaultView || null;
     if( methodName = this.GetMethod( router.controllerName, router.actionView ) ){
-      
+
     }
     else{
       if( defaultActionView !== null && ( methodName = this.GetMethod( controllerName, defaultActionView ) ) ){
@@ -173,35 +174,35 @@ export class ModuleBase{
       //Method not found 404//
       error = { code : "404", requestUri : router };
     }
-    
+
     if( error === null ){
       return controller[methodName]( contentBuffer, viewHelper );
     }
     else{
       console.log( "Error: " );
       console.log( error );
-      
+
       return false;
     }
-    
+
     return false;
-    
+
   }
-  
+
   GetLayout( layoutName ){
     return this.GetViewByType( layoutName, "layouts" );
   }
-  
+
   GetPartial( partialName, searchSharedService = true, searchSharedHost = true, searchHostSharedModule = true, searchLocalModule = true, moduleRef = {} ){
     return this.GetViewByType( partialName.toLowerCase(), "partials", searchSharedService, searchSharedHost, searchHostSharedModule, searchLocalModule, moduleRef );
   }
-  
+
   GetView( viewName ){
     let view = null;
     view = this._views["views"][viewName] || null;
     return view;
   }
-  
+
   GetViewByType( viewName, viewType, searchSharedService = true, searchSharedHost = true, searchHostSharedModule = true, searchLocalModule = true, moduleRef = {} ){
     //try to get layout for this module.
     let view = null;
@@ -225,23 +226,23 @@ export class ModuleBase{
         moduleRef.ref = this._host.service.PullHost("shared").GetModule("shared");
       }
     }
-    
+
     if( searchSharedService && view === null && this._host.service.manager.GetSharedService() !== null
-     && this._host.service !== this._host.service.manager.GetSharedService() && this._host.service.manager.GetSharedService().PullHost("shared") !== null 
+     && this._host.service !== this._host.service.manager.GetSharedService() && this._host.service.manager.GetSharedService().PullHost("shared") !== null
      && this._host.service.manager.GetSharedService().PullHost("shared").GetModule("shared") !== null) {
       view = this._host.service.manager.GetSharedService().PullHost("shared").GetModule("shared").GetViewByType(viewName, viewType);
       if( view !== null ) {
         moduleRef.ref = this._host.service.manager.GetSharedService().PullHost("shared").GetModule("shared");
       }
     }
-    
+
     return view;
   }
-  
+
   BindEvents( ){
-    
+
   }
-  
+
   _InitModels(){
     this._modelDirectory = this._moduleDirectory + "/Models/";
     for ( let database in this._service.databases ) {
@@ -251,9 +252,9 @@ export class ModuleBase{
         this._RebuildModelBase();
       }
     }
-    
+
   }
-  
+
   _RebuildModelBase( libObject ){
     let modelLib = this._coreLibrary.GetPathListener( this._modelDirectory );
     this._coreLibrary.AddPathListener( this._modelDirectory, "*", null, this._AddModelListener.bind( this ), -1 );
@@ -263,14 +264,14 @@ export class ModuleBase{
       }
     }
   }
-  
+
   _AddModelListener( pathObject, libObject ){
     let modelPath = libObject.path.replace(this._modelDirectory, "").replace(".es.js", "");
     let database = modelPath.split("/")[0];
     modelPath = modelPath.split("/")[1].toLowerCase();
     this._models[modelPath] = new libObject.uncompiled( this._service.databases[database].db, this );
   }
-  
+
   _InitViews(){
     this._viewDirectory = this._moduleDirectory + "/Views/";
     let viewLib = this._coreLibrary.GetPathListener( this._viewDirectory );
@@ -281,7 +282,7 @@ export class ModuleBase{
       }
     }
   }
-  
+
   _AddViewListener( pathObject, libObject ){
     let viewName = libObject.path.replace( this._viewDirectory, "" ).replace( ".js.html", "" ).toLowerCase();
     let viewChain = viewName.substr( 0, viewName.indexOf( "/" ) );
@@ -294,7 +295,7 @@ export class ModuleBase{
     //  this._views[viewChain][viewName] = libObject.data;
     //}
   }
-  
+
   get defaultRoutes(){
     return {
       controller : this._defaultController,
@@ -302,5 +303,5 @@ export class ModuleBase{
       event : this._defaultEvent
     }
   }
-  
+
 }
